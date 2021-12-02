5756774bc8a3bd8a0c81348c02a0fbede856b7da
lcyper <lcyper@gmail.com>
2021-12-02 20:33:11 -0300

    saque color

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:gmaj_test_app/screens/my_home_page.dart';
 import 'package:gmaj_test_app/screens/new_page.dart';
 
 void main() {
@@ -12,7 +11,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
-      color: Colors.red,
       home: NewPage(),
     );
   }
