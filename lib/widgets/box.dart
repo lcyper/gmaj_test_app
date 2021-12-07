11d8a9c07caa7d76171b38023b776d50ec320128
lcyper <lcyper@gmail.com>
2021-12-07 20:35:22 -0300

    rafactoring

--- lib/widgets/box.dart
+++ lib/widgets/box.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
 class Box extends StatelessWidget {
-  Color color;
-  Box(this.color, {Key key}) : super(key: key);
+  final Color color;
+  const Box(this.color, {Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(10),
-      padding: EdgeInsets.all(20),
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(20),
       color: color,
       height: 100,
       width: 100,
